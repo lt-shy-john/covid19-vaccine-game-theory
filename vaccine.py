1bfc332694134b4d5c5db576da9da1bc482d379b
lt-shy-john <john@shyjohn.net>
2021-08-08 21:16:02 +1000

    Create vaccine.py

---
+++ vaccine.py
@@ -0,0 +1,12 @@
+class Vaccine:
+
+    def __init__(self, name, dose, vaccine_type, cost, efficacy, alpha, beta, gamma, delta):
+        self.brand = name
+        self.dose = dose
+        self.type = vaccine_type
+        self.cost = cost
+        self.efficacy = efficacy
+        self.alpha_V = alpha
+        self.beta_V = beta
+        self.gamma_V = gamma
+        self.delta_V = delta
