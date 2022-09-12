770d5c7ad29b480e79c5324a64be91d4acc200d1
John Yeung <john@shyjohn.net>
2022-09-12 21:50:17 +1000

    Create test_mode_43.py

---
+++ test/mode/test_mode_43.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+class TestMode43(TestCase):
+    def setUp(self) -> None:
+        N = 5
+
+    def test_count_vaccine_taken(self):
+        pass
+
+    def test_count_infected_times(self):
+        pass
